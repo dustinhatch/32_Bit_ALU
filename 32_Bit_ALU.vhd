152736fd43b205abfd5b9a7c7d65c7978d1c0431
dustinhatch <73000938+dustinhatch@users.noreply.github.com>
2020-12-03 09:41:34 -0600

    Update 32_Bit_ALU.vhd

--- 32_Bit_ALU.vhd
+++ 32_Bit_ALU.vhd
@@ -32,8 +32,41 @@ if(Op_Sel = "0000") then
 
 elsif(Op_Sel = "0001") then
   if(Reg_A = x"FFFFFFFF")then
-    Cout<='1';
+    Cout <= 1';
   end if;
   ALU_Out <= std_logic_vector(unsigned(Reg_A)+1);
 
-elsif
+elsif(Op_Sel = "0010")then
+  ALU_Out <= std_logic_vector(unsigned(Reg_A)-1);'
+
+elsif(Op_Sel = "0011")then
+  if(Cin = '1')then
+    temp <= std_logic_vector('0'&unsigned(Reg_A)+unsigned('0'&Reg_B+1));
+  elsif(Cin = '0')then
+    temp <= std_logic_vector('0'&unsigned(Reg_A)+unsigned('0'&Reg_B));
+  end if;
+Cout <= temp(32);
+ALU_Out <= temp(31 downto 0);
+
+elsif(Op_Sel = "0100")then
+  ALU_Out <= NOT(Reg_A);
+
+elsif(Op_Sel = "0101")then
+  ALU_Out <= Reg_A AND Reg_B;
+
+elsif(Op_Sel = "0110")then
+  ALU_Out <= Reg_A OR Reg_B;
+
+elsif(Op_Sel = "0111")then
+  ALU_Out <= Reg_A XOR Reg_B;
+ 
+elsif(Op_Sel(3 downto 2) = "10")then
+   ALU_Out <= std_logic_vector(shift_right(unsigned(Reg_A)to_integer(unsigned(Reg_B))));
+  
+elsif(Op_Sel(3 downto 2) = "11")then
+   ALU_Out <= std_logic_vector(shift_left(unsigned(Reg_A)to_integer(unsigned(Reg_B))));
+   if(Reg_A(32-to_integer(unsigned(Reg_B)))='1')then
+     Cout <= '1';
+   end if;
+end process;
+end Behavioral; 
